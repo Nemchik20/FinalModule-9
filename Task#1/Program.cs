58ab38b05e08320ff2eb68448368a1398d200092
kjk080kjk0@mail.ru <kjk080kjk0@mail.ru>
2023-11-10 08:43:37 +0600

    Добавьте файлы проекта.

---
+++ Task#1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            List<Exception> ExceptionList = new List<Exception>() 
+            {
+                new DirectoryNotFoundException(), // Недопустимая часть пути каталога 
+                new DivideByZeroException(),//Нельзя делить на ноль
+                new DriveNotFoundException(), // диск не доступен
+                new FileNotFoundException(), // Файл не существует
+                new FormatException() // Значение находиться не в формате
+            };
+            foreach(Exception ex in ExceptionList)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch(DirectoryNotFoundException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+                catch (DivideByZeroException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+                catch (DriveNotFoundException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+                catch(FileNotFoundException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+                catch(FormatException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Task#2/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Program
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            string[] LastNames = new string[]
+            {
+                "а - Сидоренко", // используем для нагрлядности алфавит, не мог придумать фамилии )
+                "б - Сидорчук",
+                "в - Иванов",
+                "г - Человеков",
+                "д - Тут должна быть файша фамилия))"
+            };
+            Sort st = new Sort();
+            st.SortEvent += PrintsNames;
+            while (true) // главный метод)
+            {
+                try
+                {
+                    st.Read(LastNames); 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Не корректные данные");
+                }
+            }
+        }
+
+        public static void PrintsNames (string[] names) // Вывод имен в консоль
+        {
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+
+    class Sort
+    {
+        public delegate void SortDelegate(string[] names);
+        public event SortDelegate SortEvent;
+
+        public void Read(string[] names) // чтение с консоли
+        {
+            Console.WriteLine("Введите число 1 или 2 для выбора сортировки");
+            byte i = byte.Parse(Console.ReadLine());
+            if (i != 1 && i != 2) throw new FormatException(); // Выброс исключене 
+            SortLastNames(names, i); // вызов сортировки
+        }
+        public void SortLastNames(string[] lastNames, byte i) // тут сортировка и сообщение что произощло событие
+        {
+            switch(i)
+            {
+                case 1:
+                    Array.Sort(lastNames);
+                    SortEvent.Invoke(lastNames);
+                    break;
+                case 2:
+                    Array.Sort(lastNames, (x, y) => string.Compare(y, x));
+                    SortEvent.Invoke(lastNames);
+                    break;
+            }
+        }
+    }
+}
